02544a089c9d72b13d6c93b3d903b58715308c8c
袁媛 <1520793920@qq.com>
2017-03-30 23:28:06 +0800

    xml

---
+++ src/main/java/yy/eat/service/FoodService.java
@@ -0,0 +1,13 @@
+/**
+ * Created by 袁媛 on 2017/3/30.
+ */
+package yy.eat.service;
+
+/**
+ * @author yuan.yuan01@hand-china.com
+ * @name: FoodService
+ * @Description: (描述此类的功能)
+ * @date 2017年03月30日 23:26
+ */
+public interface FoodService {
+}
